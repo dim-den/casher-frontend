d50b7a8f9d0dca38035054077982d0c4c334d9ef
Dmitry Den <dmitry.denisuyk@soxes.ch>
2023-04-06 23:36:48 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutes, AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {AdminLayoutComponent} from "./layouts/admin-layout/admin-layout.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {RouterModule} from "@angular/router";
+import {SidebarModule} from "./sidebar/sidebar.module";
+import {NavbarModule} from "./shared/navbar/navbar.module";
+import {FooterModule} from "./shared/footer/footer.module";
+import {FixedPluginModule} from "./shared/fixedplugin/fixedplugin.module";
+import { ToastrModule } from "ngx-toastr";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(AppRoutes,{
+    }),
+    SidebarModule,
+    NavbarModule,
+    ToastrModule.forRoot(),
+    FooterModule,
+    FixedPluginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
